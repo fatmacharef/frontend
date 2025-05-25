1000008e1343ffa9edca8aefef4cf4d5ebeb316b
fatmacharef <129339658+fatmacharef@users.noreply.github.com>
2025-05-25 19:21:07 +0100

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,12 +9,12 @@ function Home() {
   const [user, setUser] = useState(null);
   const [showModal, setShowModal] = useState(false);
   const navigate = useNavigate();
-  const { t } = useTranslation(); // Hook de traduction
+  const { t } = useTranslation();
 
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
       if (currentUser) {
-        navigate("/chat"); // Redirige directement si connecté
+        navigate("/chat");
       } else {
         setUser(null);
       }
@@ -23,29 +23,39 @@ function Home() {
   }, [navigate]);
 
   return (
-    <div className="home-container" dir="auto">
-      <h1>
-        {t("home.welcome")} <span className="highlight">PsyBot</span>
-      </h1>
-      <p>{t("home.description")}</p>
-      <button className="start-button" onClick={() => setShowModal(true)}>
-        {t("home.start")}
-      </button>
+    <div className="home-container" >
+      <div className="home-content">
+        <div className="home-text">
+          <h1>
+            {t("home.welcome")} <span className="highlightee">PsyBot</span>
+          </h1>
+          <p>{t("home.description")}</p>
+          <button className="starte-button" onClick={() => setShowModal(true)}>
+            {t("home.start")}
+          </button>
+        </div>
+
+        <div className="home-image">
+          <img src="/home.png" alt="PsyBot illustration" />
+        </div>
+      </div>
 
       {showModal && (
-        <div className="modal-overlay">
-          <div className="modal-content">
-            <h2>{t("home.modalTitle")}</h2>
+        <div className="modale-overlay">
+          <div className="modale-contentd">
+            <h2>
+              {t("home.welcome")} <span className="highlightee">PsyBot</span>
+            </h2>
             <p>{t("home.modalText")}</p>
-            <div className="modal-buttons">
-              <button onClick={() => navigate("/inscrire")} className="signupe-button">
+            <div className="modalee-buttons">
+              <button onClick={() => navigate("/inscrire")} className="signupee-button">
                 {t("home.signup")}
               </button>
-              <button onClick={() => navigate("/login")} className="logine-button">
+              <button onClick={() => navigate("/login")} className="loginee-buttons">
                 {t("home.login")}
               </button>
             </div>
-            <button onClick={() => setShowModal(false)} className="close-button">
+            <button onClick={() => setShowModal(false)} className="closee-button">
               {t("home.close")}
             </button>
           </div>
